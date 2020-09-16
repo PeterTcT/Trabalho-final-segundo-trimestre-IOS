b5bb3bb135e4886960f71b5a0650708e861fa02e
PeterTcT <11800950@aluno.cotemig.com.br>
2020-09-16 09:51:24 -0300

    First commit

---
+++ TrabFinal35_34/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrabFinal35_34
+//
+//  Created by COTEMIG on 16/09/20.
+//  Copyright © 2020 PedroRodriguesPedroOliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
