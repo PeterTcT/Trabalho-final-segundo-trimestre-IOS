7a52c60ff28efb4872b77d8398c3f6b418e1de0d
PeterTcT <11800950@aluno.cotemig.com.br>
2020-09-23 09:21:46 -0300

    mds

--- TrabFinal35_34/ViewController.swift
+++ TrabFinal35_34/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func login(_ sender: Any) {
+        let emailText = email.text ?? ""
+        let passwordText = password.text ?? ""
+        if(emailText.isEmpty || passwordText.isEmpty){
+           let errorAlert = UIAlertController(title: "Erro", message: "No deixe nenhum campo vazio", preferredStyle: UIAlertController.Style.alert)
+            errorAlert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+            self.present(errorAlert,animated: true)
+        }else{
+            let url = URL()
+            URLSession
+        }
+    }
 }
 
