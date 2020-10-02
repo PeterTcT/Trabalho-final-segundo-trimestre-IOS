8bc539383bd0a7f488774965fb90a844a3449b00
PeterTcT <pedrorodriguesfereira@gmail.com>
2020-10-01 22:50:29 -0300

    something change...

--- TrabFinal35_34/ViewController.swift
+++ TrabFinal35_34/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let userAuthStatus = UserDefaults.standard.bool(forKey: "LoginStatus")
+        if userAuthStatus {
+            self.performSegue(withIdentifier: "login", sender: self)
+        }
         // Do any additional setup after loading the view.
     }
 
@@ -25,14 +29,28 @@ class ViewController: UIViewController {
     @IBAction func login(_ sender: Any) {
         let emailText = email.text ?? ""
         let passwordText = password.text ?? ""
-        if(emailText.isEmpty || passwordText.isEmpty){
-           let errorAlert = UIAlertController(title: "Erro", message: "No deixe nenhum campo vazio", preferredStyle: UIAlertController.Style.alert)
+        if emailText.isEmpty || passwordText.isEmpty {
+           let errorAlert = UIAlertController(title: "Erro", message: "Nao deixe nenhum campo vazio", preferredStyle: UIAlertController.Style.alert)
             errorAlert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
             self.present(errorAlert,animated: true)
-        }else{
-            let url = URL()
-            URLSession
+        } else {
+            let session = URLSession.shared
+            let url = URL(string: "https://trab-ios.herokuapp.com/login/\(emailText)/\(passwordText)")!
+            session.dataTask(with: url) { (data, response, errr) in
+                DispatchQueue.main.async {
+                    if let httpResponse = response as? HTTPURLResponse {
+                        if(httpResponse.statusCode == 200){
+                            let saveData = UserDefaults.standard
+                            saveData.set(true, forKey: "LoginStatus")
+                            self.performSegue(withIdentifier: "login", sender: self)
+                        }else{
+                            let errorAlert = UIAlertController(title: "Erro", message: "Email ou senha incorretos", preferredStyle: UIAlertController.Style.alert)
+                            errorAlert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+                            self.present(errorAlert,animated: true)                    }
+                    }
+                }
+            }.resume()
+          }
         }
     }
-}
 
